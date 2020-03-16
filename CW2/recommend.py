2ecf8e0b645920b5d8b5ff21115bbb9c36e48a11
odhran456 <44904580+odhran456@users.noreply.github.com>
2020-03-16 17:21:38 +0000

    Update recommend.py

--- CW2/recommend.py
+++ CW2/recommend.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from normalise_user_item_matrix import linebreak
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
 
 
 matrix = pd.read_csv('user_item_matrix_normalised.csv')
@@ -10,6 +11,7 @@ drop_columns_all_except_name = [i for i in range(linebreak[0],linebreak[5]+1)]
 drop_list = matrix.columns[drop_columns_all_except_name].tolist()
 name_matrix = matrix.drop(drop_list,axis=1)
 name_dict = dict(name_matrix.values)
+# print(name_dict)
 
 drop_columns = [i for i in range(1,linebreak[1]+1)]
 drop_list = matrix.columns[drop_columns].tolist()
@@ -20,16 +22,33 @@ y = pd.DataFrame(taxonomy_matrix['ID'])
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=1,shuffle=False)
 
-knn = KNeighborsClassifier(n_neighbors=3,metric='correlation')
-knn.fit(X_train,y_train.values.ravel())
+knn = NearestNeighbors(n_neighbors=8,algorithm='brute',metric='cosine')
+knn.fit(X_train)
 
-neighbours = knn.kneighbors(X_test, return_distance=False)
+neighbours = knn.kneighbors(X_test,return_distance=False)
 
+neighbour_1_posn = neighbours[0][0]
 neighbour_1_ID = y_train.iloc[neighbours[0][0]].values[0]
 neighbour_1_name = name_dict[neighbour_1_ID]
+neighbour_2_posn = neighbours[0][1]
 neighbour_2_ID = y_train.iloc[neighbours[0][1]].values[0]
 neighbour_2_name = name_dict[neighbour_2_ID]
+neighbour_3_posn = neighbours[0][2]
 neighbour_3_ID = y_train.iloc[neighbours[0][2]].values[0]
 neighbour_3_name = name_dict[neighbour_3_ID]
 
 print(neighbour_1_name,neighbour_2_name,neighbour_3_name)
+
+target = X.iloc[8].to_numpy().reshape(1,12)
+
+top_sim = round(cosine_similarity(target,X.iloc[neighbour_1_posn].to_numpy().reshape(1,12))[0][0],3)
+second_sim = round(cosine_similarity(target,X.iloc[neighbour_2_posn].to_numpy().reshape(1,12))[0][0],3)
+third_sim = round(cosine_similarity(target,X.iloc[neighbour_3_posn].to_numpy().reshape(1,12))[0][0],3)
+
+neighbours_similarity_dict = {
+    neighbour_1_name:top_sim,
+    neighbour_2_name:second_sim,
+    neighbour_3_name:third_sim
+}
+
+print(neighbours_similarity_dict)
